daecd1fafcd0956a0a68c1a9a50e427dc6699cfd
Vinícius Fernando <viniciusfernando850@gmail.com>
2025-11-22 22:10:08 -0300

    Início do Projeto
    Classes e atributos definidos, construtores e métodos geters e seters criados.

--- src/Sistema.java
+++ src/Sistema.java
@@ -5,5 +5,6 @@ public class Sistema {
     public static void main(String[] args){
         Banco banco = new Banco("Inter S.A.", 30, 20);
 
+        System.out.println("========================================");
     }
 }
